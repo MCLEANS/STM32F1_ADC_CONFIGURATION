23373fcb4bbdd65af8b594abeb0e3a991d8e99d0
MCLEANS <jackagwaya@gmail.com>
2020-07-31 20:12:27 +0300

    ADC initialization

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -12,13 +12,17 @@
 #include "stm32f10x.h"
 #include "ADC.h"
 
+custom_libraries::_ADC potentiometer(ADC1,GPIOA,0,custom_libraries::ch0,custom_libraries::SLOW);
+
 
 
 int main(void)
 {
+	potentiometer.initialize();
 
-
-
+	//SET ADC NVIC PRIORITY
+	NVIC_SetPriority(ADC1_2_IRQn,0x03);
+	NVIC_EnableIRQ(ADC1_2_IRQn);
 
 	while(1){
 
