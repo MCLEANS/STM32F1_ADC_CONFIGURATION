862e06f1f164f3b13863895054c26645c5265f18
MCLEANS <jackagwaya@gmail.com>
2020-07-31 20:17:11 +0300

    Added ADC interrupt handler

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -12,9 +12,20 @@
 #include "stm32f10x.h"
 #include "ADC.h"
 
+uint16_t potentiometer_value = 0;
+
 custom_libraries::_ADC potentiometer(ADC1,GPIOA,0,custom_libraries::ch0,custom_libraries::SLOW);
 
-
+extern "C" void ADC1_2_IRQHnadler(void){
+	//CHECK THAT WE ARE HERE BECAUSE OF ADC1 EOC
+	if(ADC1->SR & ADC_SR_EOC){
+		//CLEAR THE FLAG
+		ADC1->SR &= ~ADC_SR_EOC;
+		potentiometer_value = ADC1->DR;
+		//START THE NEXT CONVERSION
+		ADC1->CR2 |= ADC_CR2_SWSTART;
+	}
+}
 
 int main(void)
 {
